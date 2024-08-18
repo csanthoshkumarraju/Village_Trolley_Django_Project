fcd5f0f5ddd6afecbff441dd0f7e49fb9df36d83
cskraju <cskraju07@gmail.com>
2024-08-18 17:29:20 +0530

    changed Database

--- Village_Trolley_Shop_Customer_project/Village_Trolley_Shop_Customer_project/settings.py
+++ Village_Trolley_Shop_Customer_project/Village_Trolley_Shop_Customer_project/settings.py
@@ -91,12 +91,19 @@ WSGI_APPLICATION = 'Village_Trolley_Shop_Customer_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
